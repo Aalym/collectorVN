232cae1b8d36bf4eb1ccab9ed026748961e68045
serendipity1y <bkmbeshka@gmail.com>
2025-10-05 10:56:51 +0600

    Переработан characterbox

--- src/components/character-1/CharacterBox.jsx
+++ src/components/character-1/CharacterBox.jsx
@@ -4,7 +4,7 @@ import styles from "../character-1/CharacterBox.module.css";
 export default function CharacterBox({ src, alt }) {
   return (
     <div className={styles.characterBox}>
-      <img src={src} alt={alt} style={{maxWidth:"320px",maxHeight:"320px"}} />
+      <img src={src} alt={alt} style={{maxWidth:"1080px",maxHeight:"1920px"}} />
     </div>
   );
 }
